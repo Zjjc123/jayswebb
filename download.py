635a60991a83de9c99b2deed7957390c7501a06c
Casey <caseyculbertson1@gmail.com>
2023-01-17 22:59:40 -0800

    better preview downloader

--- download.py
+++ download.py
@@ -1,6 +1,11 @@
 from astroquery.mast import Observations
 
-obs_table = Observations.query_criteria(target_name='NGC 3324',obs_collection='JWST')
+# 'NGC 3324': Carnia
+# 'NGC 3132': Southern Ring Nebula
+target_name = 'NGC 3324'
+
+obs_table = Observations.query_criteria(target_name=target_name,obs_collection='JWST')
+
 print(obs_table['obs_id'])
 
 data_products = Observations.get_product_list(obs_table)
@@ -8,4 +13,6 @@ data_products = Observations.get_product_list(obs_table)
 # Observations.filter_products(data_products,productType='SCIENCE',calib_level=[3],productSubGroupDescription='I2D')
 preview_products = Observations.filter_products(data_products,productType='PREVIEW',calib_level=[3])
 
-Observations.download_products(preview_products,download_dir='downloads',flat=True)
+import os
+os.mkdir(target_name)
+Observations.download_products(preview_products,download_dir=target_name,flat=True)
